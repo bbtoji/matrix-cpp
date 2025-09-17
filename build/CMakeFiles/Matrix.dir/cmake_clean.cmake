bdf7c6834a40f9d4f0d8e309cf6243a180405fdd
bbtoji <mrmagistrr@gmail.com>
2025-09-17 16:31:02 +0400

    Initial commit

---
+++ build/CMakeFiles/Matrix.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Matrix.dir/link.d"
+  "CMakeFiles/Matrix.dir/main.cpp.o"
+  "CMakeFiles/Matrix.dir/main.cpp.o.d"
+  "Matrix"
+  "Matrix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
